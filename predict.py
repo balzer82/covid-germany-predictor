4b4989454322bd72393098b39fe861ac52a993b4
Balzer82 <balzer82@gmail.com>
2020-03-21 20:25:40 +0100

    Change Plot Y-Axis Text

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python#!/usr/bin/python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import pandas as pd
@@ -20,7 +20,7 @@ plt.style.use('bmh')
 # 
 # We are using the Covid-19 Dataset: https://github.com/CSSEGISandData/COVID-19
 
-# In[2]:
+# In[3]:
 
 
 url = 'https://raw.githubusercontent.com'
@@ -29,13 +29,13 @@ url += '/master/csse_covid_19_data/csse_covid_19_time_series'
 url += '/time_series_19-covid-Confirmed.csv'
 
 
-# In[3]:
+# In[4]:
 
 
 confirmed = pd.read_csv(url)
 
 
-# In[4]:
+# In[5]:
 
 
 confirmed.head()
@@ -43,7 +43,7 @@ confirmed.head()
 
 # ### Preprocessing
 
-# In[5]:
+# In[6]:
 
 
 ger_confirmed = confirmed[confirmed['Country/Region']=='Germany'].T
@@ -51,20 +51,20 @@ ger_confirmed = ger_confirmed[4:].astype('int')
 ger_confirmed.columns = ['confirmed']
 
 
-# In[6]:
+# In[7]:
 
 
 ger_confirmed.index = pd.to_datetime(ger_confirmed.index)
 ger_confirmed = ger_confirmed.asfreq('D')
 
 
-# In[7]:
+# In[8]:
 
 
 ger_confirmed = ger_confirmed[ger_confirmed.confirmed>100]
 
 
-# In[8]:
+# In[9]:
 
 
 today = ger_confirmed.index[-1]
@@ -72,13 +72,13 @@ today = ger_confirmed.index[-1]
 
 # ## Feature
 
-# In[9]:
+# In[10]:
 
 
 ger_confirmed['days'] = (ger_confirmed.index - ger_confirmed.index.min()).days
 
 
-# In[10]:
+# In[11]:
 
 
 ger_confirmed.head()
@@ -86,14 +86,14 @@ ger_confirmed.head()
 
 # ## Prediction Model
 
-# In[11]:
+# In[12]:
 
 
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
 
-# In[12]:
+# In[13]:
 
 
 X = ger_confirmed['days'].values.reshape(-1, 1)
@@ -103,21 +103,21 @@ logy = np.log(y)
 
 # ### Train
 
-# In[13]:
+# In[14]:
 
 
 clf = LinearRegression()
 clf.fit(X, logy)
 
 
-# In[14]:
+# In[15]:
 
 
 logy_pred = clf.predict(X)
 ger_confirmed['predicted'] = np.exp(logy_pred).astype('int')
 
 
-# In[15]:
+# In[16]:
 
 
 ger_confirmed.tail()
@@ -125,7 +125,7 @@ ger_confirmed.tail()
 
 # ## Save the model for later use
 
-# In[16]:
+# In[17]:
 
 
 import pickle
@@ -136,13 +136,13 @@ with open('%s-Germany-Covid19-Prediction-Model.pkl' % today.strftime('%Y-%m-%d')
 
 # ## Future
 
-# In[17]:
+# In[18]:
 
 
 fd = 13 # days into the future
 
 
-# In[18]:
+# In[19]:
 
 
 # Create DataFrame in the Future
@@ -152,7 +152,7 @@ days_in_future = ger_confirmed.days[-1] + np.arange(1, fd)
 future = pd.DataFrame(data=days_in_future, index=dates, columns=['days'])
 
 
-# In[19]:
+# In[20]:
 
 
 ger_future = ger_confirmed.append(future, sort=True)
@@ -160,28 +160,34 @@ ger_future = ger_confirmed.append(future, sort=True)
 
 # ### Predict the Future
 
-# In[20]:
+# In[21]:
 
 
 X_future = ger_future['days'].values.reshape(-1, 1)
 
 
-# In[21]:
+# In[22]:
 
 
 logy_pred = clf.predict(X_future)
 ger_future['predicted'] = np.exp(logy_pred).astype('int')
 
 
+# In[28]:
+
+
+ger_future
+
+
 # ## Future Plot
 
-# In[22]:
+# In[23]:
 
 
 title = 'Bestätigte Fälle und Vorhersage für Deutschland (Basierend auf CSSE COVID-19 Dataset)'
 
 
-# In[23]:
+# In[24]:
 
 
 ax = ger_future['confirmed'].plot(label='Bestätigte COVID-19 Fälle', marker='o')
@@ -189,7 +195,7 @@ ax = ger_future['predicted'].plot(label='exponentielles Wachstum\n(Modell vom %s
                                   alpha=0.6, ax=ax)
 
 ax.legend()
-ax.set_ylabel('Personen')
+ax.set_ylabel('Log(Anzahl)')
 ax.set_yscale('log')
 ax.set_title(title, fontsize=8)
 ax.annotate('unter CC-BY 2.0 Lizenz Paul Balzer', xy=(.5, 0.02), xycoords='figure fraction', ha='center', fontsize=6, color='gray')
@@ -200,7 +206,7 @@ plt.savefig('./%s-Germany-Covid19-Prediction.png' % today.strftime('%Y-%m-%d'), 
 
 # ## Export as Excel
 
-# In[24]:
+# In[25]:
 
 
 ger_future.to_excel('./%s-Germany-Covid19-Prediction.xlsx' % today.strftime('%Y-%m-%d'))
@@ -210,7 +216,7 @@ ger_future.to_excel('./%s-Germany-Covid19-Prediction.xlsx' % today.strftime('%Y-
 # 
 # We are using Bokeh to export an interactive website
 
-# In[25]:
+# In[26]:
 
 
 from bokeh.plotting import figure
@@ -220,7 +226,7 @@ from bokeh.layouts import column
 from bokeh.io import output_file, save
 
 
-# In[26]:
+# In[27]:
 
 
 p = figure(tools="hover,save,pan,box_zoom,reset,wheel_zoom",
