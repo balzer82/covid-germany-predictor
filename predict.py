9200f56236e65f21b3a2bf9a1ddd0afcafe301ee
Balzer82 <balzer82@gmail.com>
2020-03-21 18:08:05 +0100

    Add Excel Output

--- predict.py
+++ predict.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python#!/usr/bin/python
 # coding: utf-8
 
-# In[78]:
+# In[1]:
 
 
-from bokeh.io import output_file, save
-from bokeh.layouts import column
-from bokeh.models import Div
-from bokeh.models import BoxAnnotation
-from bokeh.models.formatters import DatetimeTickFormatter
-from bokeh.plotting import figure
-import pickle
-import numpy as np
-from sklearn.linear_model import LinearRegression
 import pandas as pd
 
 
@@ -21,15 +12,15 @@ plt.style.use('bmh')
 
 
 # # WirVsVirus Hackathon
-#
+# 
 # Die entscheidende Frage bei der Beurteilung aller Maßnahmen ist, ob das exponentielle Wachstum verlangsamt worden ist, d.h. die exponentielle Wachstumskurve abflacht.
 # Dazu macht man am besten anhand bestehender Daten ein Modell-Fit und schaut, ob aktuelle Fallzahlen das Modell überschreiten oder man mit den Fallzahlen darunter bleibt.
 
 # ## Download Data from CSSE COVID-19 Dataset
-#
+# 
 # We are using the Covid-19 Dataset: https://github.com/CSSEGISandData/COVID-19
 
-# In[79]:
+# In[2]:
 
 
 url = 'https://raw.githubusercontent.com'
@@ -38,13 +29,13 @@ url += '/master/csse_covid_19_data/csse_covid_19_time_series'
 url += '/time_series_19-covid-Confirmed.csv'
 
 
-# In[80]:
+# In[3]:
 
 
 confirmed = pd.read_csv(url)
 
 
-# In[81]:
+# In[4]:
 
 
 confirmed.head()
@@ -52,28 +43,28 @@ confirmed.head()
 
 # ### Preprocessing
 
-# In[82]:
+# In[5]:
 
 
-ger_confirmed = confirmed[confirmed['Country/Region'] == 'Germany'].T
+ger_confirmed = confirmed[confirmed['Country/Region']=='Germany'].T
 ger_confirmed = ger_confirmed[4:].astype('int')
 ger_confirmed.columns = ['confirmed']
 
 
-# In[83]:
+# In[6]:
 
 
 ger_confirmed.index = pd.to_datetime(ger_confirmed.index)
 ger_confirmed = ger_confirmed.asfreq('D')
 
 
-# In[84]:
+# In[7]:
 
 
-ger_confirmed = ger_confirmed[ger_confirmed.confirmed > 100]
+ger_confirmed = ger_confirmed[ger_confirmed.confirmed>100]
 
 
-# In[85]:
+# In[8]:
 
 
 today = ger_confirmed.index[-1]
@@ -81,13 +72,13 @@ today = ger_confirmed.index[-1]
 
 # ## Feature
 
-# In[86]:
+# In[9]:
 
 
 ger_confirmed['days'] = (ger_confirmed.index - ger_confirmed.index.min()).days
 
 
-# In[87]:
+# In[10]:
 
 
 ger_confirmed.head()
@@ -95,10 +86,14 @@ ger_confirmed.head()
 
 # ## Prediction Model
 
-# In[88]:
+# In[11]:
 
 
-# In[89]:
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+
+# In[12]:
 
 
 X = ger_confirmed['days'].values.reshape(-1, 1)
@@ -108,14 +103,14 @@ logy = np.log(y)
 
 # ### Train
 
-# In[90]:
+# In[13]:
 
 
 clf = LinearRegression()
 clf.fit(X, logy)
 
 
-# In[91]:
+# In[14]:
 
 
 logy_pred = clf.predict(X)
@@ -124,8 +119,10 @@ ger_confirmed['predicted'] = np.exp(logy_pred).astype('int')
 
 # ## Save the model for later use
 
-# In[95]:
+# In[18]:
 
+
+import pickle
 
 with open('%s-Germany-Covid19-Prediction-Model.pkl' % today.strftime('%Y-%m-%d'), 'wb') as f:
     pickle.dump(clf, f)
@@ -133,13 +130,13 @@ with open('%s-Germany-Covid19-Prediction-Model.pkl' % today.strftime('%Y-%m-%d')
 
 # ## Future
 
-# In[96]:
+# In[19]:
 
 
-fd = 13  # days into the future
+fd = 13 # days into the future
 
 
-# In[97]:
+# In[20]:
 
 
 # Create DataFrame in the Future
@@ -149,7 +146,7 @@ days_in_future = ger_confirmed.days[-1] + np.arange(1, fd)
 future = pd.DataFrame(data=days_in_future, index=dates, columns=['days'])
 
 
-# In[98]:
+# In[21]:
 
 
 ger_future = ger_confirmed.append(future, sort=True)
@@ -157,13 +154,13 @@ ger_future = ger_confirmed.append(future, sort=True)
 
 # ### Predict the Future
 
-# In[99]:
+# In[22]:
 
 
 X_future = ger_future['days'].values.reshape(-1, 1)
 
 
-# In[100]:
+# In[23]:
 
 
 logy_pred = clf.predict(X_future)
@@ -172,13 +169,13 @@ ger_future['predicted'] = np.exp(logy_pred).astype('int')
 
 # ## Future Plot
 
-# In[101]:
+# In[24]:
 
 
 title = 'Bestätigte Fälle und Vorhersage für Deutschland (Basierend auf CSSE COVID-19 Dataset)'
 
 
-# In[102]:
+# In[25]:
 
 
 ax = ger_future['confirmed'].plot(label='Bestätigte Fälle', marker='o')
@@ -189,21 +186,36 @@ ax.legend()
 ax.set_ylabel('Personen')
 ax.set_yscale('log')
 ax.set_title(title, fontsize=8)
-ax.annotate('unter CC-BY 2.0 Lizenz Paul Balzer', xy=(.5, 0.02),
-            xycoords='figure fraction', ha='center', fontsize=6, color='gray')
+ax.annotate('unter CC-BY 2.0 Lizenz Paul Balzer', xy=(.5, 0.02), xycoords='figure fraction', ha='center', fontsize=6, color='gray')
 
 plt.tight_layout()
 plt.savefig('./%s-Germany-Covid19-Prediction.png' % today.strftime('%Y-%m-%d'), dpi=150)
-plt.close()
+
+
+# ## Export as Excel
+
+# In[26]:
+
+
+ger_future.to_excel('./%s-Germany-Covid19-Prediction.xlsx' % today.strftime('%Y-%m-%d'))
+
 
 # # Interactive Website
-#
+# 
 # We are using Bokeh to export an interactive website
 
-# In[103]:
+# In[27]:
 
 
-# In[104]:
+from bokeh.plotting import figure
+from bokeh.models.formatters import DatetimeTickFormatter
+from bokeh.models import BoxAnnotation
+from bokeh.models import Div
+from bokeh.layouts import column
+from bokeh.io import output_file, save
+
+
+# In[28]:
 
 
 p = figure(plot_width=1280, plot_height=720,
@@ -216,21 +228,21 @@ p.line(ger_future.index, ger_future.predicted, line_width=5,
 p.circle(ger_confirmed.index, ger_confirmed.confirmed,
          fill_color="white", size=12, legend='Bestätigte Fälle')
 
-p.xaxis.formatter = DatetimeTickFormatter(
+p.xaxis.formatter=DatetimeTickFormatter(
     years="%d.%m.%Y",
     months="%d.%m.%Y",
     days="%A %d.%m.%Y",
 )
 
 gray_box = BoxAnnotation(left=ger_confirmed.index[0],
-                         right=ger_confirmed.index[-1],
-                         fill_color='gray', fill_alpha=0.1)
+                          right=ger_confirmed.index[-1],
+                          fill_color='gray', fill_alpha=0.1)
 p.add_layout(gray_box)
 
 p.legend.location = "top_left"
 
 div = Div(text="""Quellcode: <a href="https://github.com/balzer82/covid-germany-predictor">Covid Germany Predictor</a> unter CC-BY2.0 Lizenz on Github.""",
-          width=600, height=100)
+width=600, height=100)
 
 output_file("index.html")
 save(column(p, div))
