87177776674e12b0c638284958af0fbc616e9441
Balzer82 <balzer82@gmail.com>
2020-03-22 12:21:25 +0100

    Updated Model and styling

--- predict.py
+++ predict.py
@@ -5,6 +5,8 @@
 
 
 import pandas as pd
+import time
+import datetime
 
 
 import matplotlib.pyplot as plt
@@ -27,19 +29,19 @@ url = 'https://raw.githubusercontent.com'
 url += '/CSSEGISandData/COVID-19'
 url += '/master/csse_covid_19_data/csse_covid_19_time_series'
 url += '/time_series_19-covid-Confirmed.csv'
-print('Downloading from %s' % url)
+print('Downloading Data from %s' % url)
 
 
 # In[3]:
 
 
-confirmed = pd.read_csv(url)
+confirmed_raw = pd.read_csv(url)
 
 
 # In[4]:
 
 
-confirmed.head()
+confirmed_raw.head()
 
 
 # ### Preprocessing
@@ -47,30 +49,35 @@ confirmed.head()
 # In[5]:
 
 
-ger_confirmed = confirmed[confirmed['Country/Region']=='Germany'].T
-ger_confirmed = ger_confirmed[4:].astype('int')
-ger_confirmed.columns = ['confirmed']
+confirmed = confirmed_raw[confirmed_raw['Country/Region']=='Germany'].T
+confirmed = confirmed[4:].astype('int')
+confirmed.columns = ['confirmed']
 
 
 # In[6]:
 
 
-ger_confirmed.index = pd.to_datetime(ger_confirmed.index)
-ger_confirmed = ger_confirmed.asfreq('D')
+confirmed.index = pd.to_datetime(confirmed.index)
+confirmed = confirmed.asfreq('D')
 
 
-# Filter der Daten: Wir nehmen für die Modellbildung erst den Tag als Beginn, an dem der 100. Erkrankte gemeldet wurde.
+# Filter der Daten: Wir nehmen für die Modellbildung erst den Tag als Beginn, an dem erstmals mehr als 100 Erkrankte gemeldet waren.
 
 # In[7]:
 
 
-ger_confirmed = ger_confirmed[ger_confirmed.confirmed>=100]
+confirmed = confirmed[confirmed.confirmed>=100]
 
+
+# ## Modellvarianten
+# 
+# Man kann immer ein aktuelles Modell rechnen, oder schauen wie sich die Zahlen verändern, basierend auf einem Modell von einem festen Datum aus.
 
 # In[8]:
 
 
-today = ger_confirmed.index[-1]
+#today = confirmed.index[-1] # immer aktuell rechnen
+today = datetime.date(2020, 3, 21) # 21.03.2020 als Bezugsdatum nehme
 
 
 # ## Feature
@@ -78,13 +85,13 @@ today = ger_confirmed.index[-1]
 # In[9]:
 
 
-ger_confirmed['days'] = (ger_confirmed.index - ger_confirmed.index.min()).days
+confirmed['days'] = (confirmed.index - confirmed.index.min()).days
 
 
 # In[10]:
 
 
-ger_confirmed.head()
+confirmed.head()
 
 
 # ## Ausgangssperren
@@ -94,17 +101,10 @@ ger_confirmed.head()
 # In[11]:
 
 
-import time
-import datetime
-
-
-# In[12]:
-
-
 ausgangssperren_timestamp = datetime.date(2020, 3, 21)
 
 
-# In[13]:
+# In[12]:
 
 
 ausgangssperren_timestamp_epoch = time.mktime(ausgangssperren_timestamp.timetuple())*1000
@@ -112,7 +112,7 @@ ausgangssperren_timestamp_epoch = time.mktime(ausgangssperren_timestamp.timetupl
 
 # ## Prediction Model
 # 
-# Ein exponentielles Wachstum (freie Ansteckung) verläuft nach:
+# Ein exponentielles Wachstum (freie unkontrollierte Ausbreitung) verläuft nach:
 # 
 # $y = A e^{Bx}$
 # 
@@ -120,31 +120,32 @@ ausgangssperren_timestamp_epoch = time.mktime(ausgangssperren_timestamp.timetupl
 # 
 # $\log_e(y) = B x + \log_e (A)$
 
-# In[14]:
+# In[13]:
 
 
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 import numpy as np
 
 
-# In[15]:
+# In[14]:
 
 
-X = ger_confirmed['days'].values.reshape(-1, 1)
-y = ger_confirmed['confirmed'].values
+X = confirmed[:today]['days'].values.reshape(-1, 1)
+y = confirmed[:today]['confirmed'].values
 logy = np.log(y)
 
 
 # ### Train
 
-# In[16]:
+# In[15]:
 
 
 clf = LinearRegression()
 clf.fit(X, logy)
 
 
-# In[17]:
+# In[16]:
 
 
 logy_pred = clf.predict(X)
@@ -152,26 +153,36 @@ logy_pred = clf.predict(X)
 
 # Die mit dem linearen Modell vorhergesagten Werte sind im logarithmischen, müssen mit der $e^y$ noch zurück konvertiert werden.
 
+# In[17]:
+
+
+confirmed.loc[:today,'predicted'] = np.exp(logy_pred).astype('int')
+
+
 # In[18]:
 
 
-ger_confirmed['predicted'] = np.exp(logy_pred).astype('int')
-
-
-# In[19]:
-
-
-ger_confirmed.tail()
+confirmed.tail()
 
 
 # ### Modelparameter
 
-# In[20]:
+# In[19]:
 
 
 B = clf.coef_[0]
 A = np.exp(clf.intercept_)
 print('Modellparameter sind A=%.1f, B=%.3f' % (A, B))
+
+
+# ### Model Evaluation
+# 
+# R² score: the coefficient of determination
+
+# In[20]:
+
+
+r2_score(confirmed['confirmed'].values, confirmed['predicted'].values)
 
 
 # ## Save the model for later use
@@ -181,10 +192,11 @@ print('Modellparameter sind A=%.1f, B=%.3f' % (A, B))
 
 import pickle
 
-with open('%s-Germany-Covid19-Prediction-Model.pkl' % today.strftime('%Y-%m-%d'), 'wb') as f:
+pklfilename = '%s-Germany-Covid19-Prediction-Model.pkl' % today.strftime('%Y-%m-%d')
+with open(pklfilename, 'wb') as f:
     pickle.dump(clf, f)
     
-print('Saved the Model')
+print('Saved the Model to %s' % pklfilename)
 
 
 # ## Future
@@ -192,15 +204,15 @@ print('Saved the Model')
 # In[22]:
 
 
-fd = 13 # days into the future
+fd = 12 # days into the future
 
 
 # In[23]:
 
 
 # Create DataFrame in the Future
-dates = pd.date_range(ger_confirmed.index[-1], periods=fd, closed='right')
-days_in_future = ger_confirmed.days[-1] + np.arange(1, fd)
+dates = pd.date_range(confirmed.index[-1], periods=fd, closed='right')
+days_in_future = confirmed.days[-1] + np.arange(1, fd)
 
 future = pd.DataFrame(data=days_in_future, index=dates, columns=['days'])
 
@@ -208,7 +220,7 @@ future = pd.DataFrame(data=days_in_future, index=dates, columns=['days'])
 # In[24]:
 
 
-ger_future = ger_confirmed.append(future, sort=True)
+ger_future = confirmed.append(future, sort=True)
 
 
 # ### Predict the Future
@@ -268,8 +280,9 @@ print('Saved the Figure')
 # In[30]:
 
 
-ger_future.to_excel('./%s-Germany-Covid19-Prediction.xlsx' % today.strftime('%Y-%m-%d'))
-print('Saved the Excel')
+xlsfile = './%s-Germany-Covid19-Prediction.xlsx' % today.strftime('%Y-%m-%d')
+ger_future.to_excel(xlsfile)
+print('Saved the Excel to %s' % xlsfile)
 
 
 # # Interactive Website
@@ -283,23 +296,37 @@ from bokeh.plotting import figure
 from bokeh.models.formatters import DatetimeTickFormatter
 from bokeh.models import Div, HoverTool, BoxAnnotation, Span
 from bokeh.layouts import column
-from bokeh.io import output_file, save
+from bokeh.embed import file_html
+from bokeh.resources import CDN
+from bokeh.palettes import inferno
 
 
 # In[32]:
 
 
+colors = inferno(6) # What else for this scenario ;)
+
+
+# In[38]:
+
+
 p = figure(tools="hover,save,pan,box_zoom,reset,wheel_zoom",
            x_axis_type="datetime", y_axis_type="log",
-           title=title)
+           title=title.replace(')', ' der John Hopkins University)'))
+
+# Vertical line for Ausgangssperren
+vline = Span(location=ausgangssperren_timestamp_epoch,
+             dimension='height', line_color='gray',
+             line_dash='dashed', line_width=3, name='Beginn Ausgangssperren')
+p.add_layout(vline)
 
 # Vorhersagemodell als Linie
-p.line(ger_future.index, ger_future.predicted, line_width=5, line_color='darkred',
+p.line(ger_future.index, ger_future.predicted, line_width=4, line_color=colors[3],
        legend='exponentielles Wachstum\n(Modell vom %s)' % today.strftime('%d.%m.%Y'))
 
 # Tatsächliche Fälle als Punkte
-p.circle(ger_confirmed.index, ger_confirmed.confirmed,
-         fill_color="lightblue", size=12, legend='Bestätigte COVID-19 Fälle')
+p.circle(confirmed.index, confirmed.confirmed, line_color=colors[4],
+         fill_color=colors[5], size=14, legend='Bestätigte COVID-19 Fälle')
 
 # X-Achse ordentlich formatieren
 p.xaxis.formatter=DatetimeTickFormatter(
@@ -308,9 +335,11 @@ p.xaxis.formatter=DatetimeTickFormatter(
     days="%A %d.%m.%Y",
 )
 
+p.yaxis.axis_label = 'Anzahl (Logarithmisch)'
+
 # Daten-Zeitraum
-gray_box = BoxAnnotation(left=ger_confirmed.index[0],
-                          right=ger_confirmed.index[-1],
+gray_box = BoxAnnotation(left=confirmed.index[0],
+                          right=confirmed.index[-1],
                           fill_color='gray', fill_alpha=0.1)
 p.add_layout(gray_box)
 
@@ -322,12 +351,6 @@ p.select_one(HoverTool).tooltips = [
 p.select_one(HoverTool).formatters = {'x':'datetime'}
 p.select_one(HoverTool).mode = 'vline'
 
-# Vertical line for Ausgangssperren
-vline = Span(location=ausgangssperren_timestamp_epoch,
-             dimension='height', line_color='gray',
-             line_dash='dashed', line_width=3, name='Beginn Ausgangssperren')
-p.add_layout(vline)
-
 p.toolbar.autohide = True
 
 # Legende
@@ -336,13 +359,55 @@ p.legend.location = "top_left"
 # Anmerkung
 div = Div(text="""Quellcode: <a href="https://github.com/balzer82/covid-germany-predictor">Covid Germany Predictor</a>
             unter CC-BY2.0 Lizenz von Paul Balzer on Github.
-            Disclaimer: Ich bin kein Epidemiologe oder Virologe, also kein Experte!""")
+            Disclaimer: Ich bin kein Epidemiologe oder Virologe, das ist keine offizielle Vorausberechnung! 
+            <a href="https://cbcity.de/impressum">Impressum</a>""")
 
 # Save
-output_file("./html/index.html", title='COVID-19 Prediction Germany')
+html = file_html(column(p, div, sizing_mode="stretch_both"), CDN, 'COVID-19 Prediction Germany')
 
-save(column(p, div, sizing_mode="stretch_both"))
-print('Saved the html')
+
+# ## Style the Website
+
+# In[39]:
+
+
+head = '''
+  <body>
+        <div><img src="https://wirvsvirushackathon.org/wp-content/uploads/2020/03/12-scaled.jpg" style="height: 30px; padding: 10px;"></div>
+'''
+
+
+# In[40]:
+
+
+gtc = '''
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-7230698-7"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-7230698-7');
+        </script>
+    </body>
+'''
+
+
+# In[41]:
+
+
+websitehtml = html.replace('<body>', head)
+websitehtml = websitehtml.replace('</body>', gtc)
+
+
+# In[42]:
+
+
+with open('./html/index.html', 'w') as htmlfile:
+    htmlfile.write(websitehtml.strip())
+
+print('Saved the /html/index.html')
 
 
 # CC-BY 2.0 Paul Balzer
